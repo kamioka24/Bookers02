cf6cf2a5f8227f3b57d75336125f8f843f19adc4
kamioka24 <keee2404@gmail.com>
2020-05-30 20:04:39 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/about' #要確認
-  get '/' => 'users#top'
+  get 'home/about'
+  root 'users#top'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
